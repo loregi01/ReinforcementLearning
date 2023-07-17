752e4f8662837145b9bb7679970cf637252cf806
loregi01 <gizzilorenzo@virgilio.it>
2023-07-17 17:35:05 +0200

    fix

--- QTableAttempt.py
+++ QTableAttempt.py
@@ -8,11 +8,11 @@ learning_rate = 0.01
 n_episodes = 100000
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2)
-final_epsilon = 0.1
+final_epsilon = 0.0
 eps = start_epsilon
 discount_factor = 0.95
 
-env = gym.make('MountainCar-v0')
+env = gym.make('MountainCar-v0', render_mode="human")
 
 Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
@@ -20,6 +20,8 @@ for episode in range(n_episodes):
 
     observation = env.reset()
     observation = observation[0]
+    observation[0] = round(observation[0],2)
+    observation[1] = round(observation[1],2)
     done = False
     print('episode :' + '' + str(episode))
 
@@ -30,6 +32,8 @@ for episode in range(n_episodes):
           action = np.argmax(Q[(observation[0], observation[1])])
 
         new_observation, reward, terminated, truncated, info = env.step(action)
+        new_observation[0] = round(new_observation[0],2)
+        new_observation[1] = round(new_observation[1],2)
 
         done = terminated or truncated
 
@@ -37,7 +41,7 @@ for episode in range(n_episodes):
 
         observation = new_observation
 
+    env.render()
     print(observation[0])
     eps = max(final_epsilon, eps - epsilon_decay)
-    print(eps)
-
+    #print(eps)
