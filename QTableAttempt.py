67fbda2961fb9c2ba56c067f0f6f4726aade9581
loregi01 <gizzilorenzo@virgilio.it>
2023-07-18 10:42:02 +0200

    addTrainingVisualization

--- QTableAttempt.py
+++ QTableAttempt.py
@@ -3,6 +3,7 @@ from gym import Env
 import numpy as np
 import random
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 learning_rate = 0.01
 n_episodes = 100000
@@ -12,7 +13,12 @@ final_epsilon = 0.0
 eps = start_epsilon
 discount_factor = 0.95
 
-env = gym.make('MountainCar-v0', render_mode="human")
+position_queue_t = []
+velocity_queue_t = []
+position_queue = []
+velocity_queue = []
+
+env = gym.make('MountainCar-v0')
 
 Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
@@ -22,6 +28,8 @@ for episode in range(n_episodes):
     observation = observation[0]
     observation[0] = round(observation[0],2)
     observation[1] = round(observation[1],2)
+    position_queue_t.append(observation[0])
+    velocity_queue_t.append(observation[0])
     done = False
     print('episode :' + '' + str(episode))
 
@@ -34,14 +42,64 @@ for episode in range(n_episodes):
         new_observation, reward, terminated, truncated, info = env.step(action)
         new_observation[0] = round(new_observation[0],2)
         new_observation[1] = round(new_observation[1],2)
+        position_queue_t.append(new_observation[0])
+        velocity_queue_t.append(new_observation[1])
 
         done = terminated or truncated
 
         Q[(observation[0],observation[1])][action] += learning_rate * (reward + discount_factor * np.max(Q[(new_observation[0],new_observation[1])]) - Q[(observation[0], observation[1])][action])
 
         observation = new_observation
-
-    env.render()
-    print(observation[0])
+        
     eps = max(final_epsilon, eps - epsilon_decay)
     #print(eps)
+
+for episode in range(n_episodes):
+  observation = env.reset()
+  observation = observation[0]
+  observation[0] = round(observation[0],2)
+  observation[1] = round(observation[1],2)
+  done = False
+  print('episode :' + '' + str(episode))
+  position_queue.append(observation[0])
+  velocity_queue.append(observation[1])
+
+  while not done:
+    action = np.argmax(Q[(observation[0], observation[1])])
+    
+    new_observation, reward, terminated, truncated, info = env.step(action)
+    
+    position_queue.append(new_observation[0])
+    velocity_queue.append(new_observation[1])
+    
+    new_observation[0] = round(new_observation[0],2)
+    new_observation[1] = round(new_observation[1],2)
+    
+    done = terminated or truncated
+    
+    observation = new_observation
+
+rolling_length = 500
+fig, axs = plt.subplots(ncols=2, figsize=(15, 5))
+
+axs[0].set_title("Positions Per Episode (Training)")
+episode_position_t = np.convolve(np.array(position_queue_t).flatten(), np.ones(rolling_length), mode="valid")/rolling_length
+axs[0].plot(range(len(episode_position_t)), episode_position_t)
+
+axs[1].set_title("Cart Velocity Per Episode (Training)")
+episode_velocity_t = np.convolve(np.array(velocity_queue_t).flatten(), np.ones(rolling_length), mode="valid")/rolling_length
+axs[1].plot(range(len(episode_velocity_t)), episode_velocity_t)
+
+plt.show()
+
+fig2, axs2 = plt.subplots(ncols=2, figsize=(15, 5))
+
+axs2[0].set_title("Positions Per Episode")
+episode_position = np.convolve(np.array(position_queue).flatten(), np.ones(rolling_length), mode="valid")/rolling_length
+axs2[0].plot(range(len(episode_position)), episode_position)
+
+axs2[1].set_title("Cart Velocity Per Episode")
+episode_velocity = np.convolve(np.array(velocity_queue).flatten(), np.ones(rolling_length), mode="valid")/rolling_length
+axs2[1].plot(range(len(episode_velocity)), episode_velocity)
+
+plt.show()
